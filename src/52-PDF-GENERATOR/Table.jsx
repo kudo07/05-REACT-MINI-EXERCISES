c66fcff1d776eb7af4052ba623bee244669953b1
kudo07 <134780035+kudo07@users.noreply.github.com>
2024-05-05 08:54:17 +0530

    debug complete

--- src/52-PDF-GENERATOR/Table.jsx
+++ src/52-PDF-GENERATOR/Table.jsx
@@ -1,8 +1,12 @@
 import { jsPDF } from 'jspdf';
+import { useState } from 'react';
 
 function Table() {
   const jspdf = new jsPDF('p', 'pt', 'letter');
-
+  const [val1, setVal1] = useState('');
+  const handleChange = (e) => {
+    setVal1(e.target.value);
+  };
   const handleSubmit = (e) => {
     e.preventDefault();
     const val = e.target.txt.value;
@@ -15,13 +19,16 @@ function Table() {
       autoPaging: 'text',
     };
 
-    jspdf.html(val, data);
+    jspdf.html(val + ' ' + val1, data);
+    // jspdf.html(val1, data);
   };
 
   return (
     <>
       <form onSubmit={(e) => handleSubmit(e)}>
         <textarea name="txt" /> <br />
+        <input type="text" value={val1} onChange={handleChange} />
+        <br />
         <button>Generate</button>
       </form>
     </>
